ad91fbfe164fdeb83cab7c9621d26182263b6e13
JuliMik <gusachenkojulia:gmail.com>
2024-11-19 15:26:13 +0000

    Fixed errors

--- main_task1.py
+++ main_task1.py
@@ -1,25 +1,27 @@
 import pathlib
 
-doc_salary_path = pathlib.Path('./Salary.txt')
-
 
 def total_salary(path):
     try:
-        with open(doc_salary_path, "r", encoding="utf-8") as file:
+        with open(path, "r", encoding="utf-8") as file:
             salaries = list()
             for line in file.readlines():
                 try:
-                    salaries.append(int(line.split(',')[1]))
+                    salaries.append(float(line.split(',')[1]))
                 except ValueError:
                     print("Salary isn't correct")
         salaries_sum = 0
         for value in salaries:
             salaries_sum += value
-        total_and_average_tuple = (salaries_sum, round(salaries_sum / len(salaries)))
-        return total_and_average_tuple
+        if len(salaries) > 0 :
+            total_and_average_tuple = (salaries_sum, salaries_sum / len(salaries))
+            return total_and_average_tuple
+        else:
+            total_and_average_tuple = ()
     except Exception as e:
         return f"{e}."
 
 
-total, average = total_salary(doc_salary_path)
+# Приклад використання функції
+total, average = total_salary('./Salary.txt')
 print(f'Total salary: {total}, average salary: {average}')
